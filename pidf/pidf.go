284b4525d619d244bbbf94e4ea9cb0c312d559d7
someonegg <someonegg@163.com>
2016-01-11 20:09:18 +0800

    add logf package

--- pidf/pidf.go
+++ pidf/pidf.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package idgener can generate pid file.
+// Package pidf can generate pid file.
 package pidf
 
 import (
